78737799f2db55f36861f27e6656cbcb2b0847ac
Lee <43488305+poiu694@users.noreply.github.com>
2024-05-31 20:54:42 +0900

    feat(polee-react): storybook fix

--- apps/docs/src/stories/polee.modal.stories.tsx
+++ apps/docs/src/stories/polee.modal.stories.tsx
@@ -3,11 +3,8 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 import { useState } from 'react';
 
-const ModalContent = (
-  <div style={{ padding: 10, borderRadius: 8, backgroundColor: '#fff' }}>Modal Test (polee)</div>
-);
 const meta: Meta<typeof Modal> = {
-  title: 'Components/Modal_polee',
+  title: 'polee/Modal',
   component: Modal,
   parameters: {
     layout: 'centered',
@@ -15,57 +12,72 @@ const meta: Meta<typeof Modal> = {
   tags: ['autodocs'],
   argTypes: {
     isOpen: {
-      control: 'boolean',
-      description: '모달의 열림/닫힘 상태를 제어',
+      table: {
+        control: 'boolean',
+        description: '모달의 열림/닫힘 상태를 제어',
+      },
     },
     shouldCloseOnEsc: {
-      control: 'boolean',
-      description: 'ESC 키를 누르면 모달 닫히는 여부',
+      table: {
+        control: 'boolean',
+        description: 'ESC 키를 누르면 모달 닫히는 여부',
+      },
     },
     shouldCloseOnDimClick: {
-      control: 'boolean',
-      description: '모달 외부를 클릭하면 모달을 닫히는 여부',
+      table: {
+        control: 'boolean',
+        description: '모달 외부를 클릭하면 모달을 닫히는 여부',
+      },
     },
     preventBackgroundScroll: {
-      control: 'boolean',
-      description: '모달이 열려 있을 때 배경 스크롤을 방지',
+      table: {
+        control: 'boolean',
+        description: '모달이 열려 있을 때 배경 스크롤을 방지',
+      },
     },
     enterAnimation: {
-      control: 'inline-radio',
-      options: [
-        'fade-in',
-        'slide-up',
-        'slide-down',
-        'fade-in-slide-up',
-        'fade-in-slide-down',
-        'custom-enter',
-      ],
-      description: '모달 열기 애니메이션 CSS 클래스',
+      table: {
+        control: 'inline-radio',
+        options: [
+          'fade-in',
+          'slide-up',
+          'slide-down',
+          'fade-in-slide-up',
+          'fade-in-slide-down',
+          'custom-enter',
+        ],
+        description: '모달 열기 애니메이션 CSS 클래스',
+      },
     },
     exitAnimation: {
-      control: 'inline-radio',
-      options: [
-        'fade-out',
-        'slide-up',
-        'slide-down',
-        'fade-out-slide-up',
-        'fade-out-slide-down',
-        'custom-exit',
-      ],
-      description: '모달 닫기 애니메이션 CSS 클래스',
+      table: {
+        control: 'inline-radio',
+        options: [
+          'fade-out',
+          'slide-up',
+          'slide-down',
+          'fade-out-slide-up',
+          'fade-out-slide-down',
+          'custom-exit',
+        ],
+        description: '모달 닫기 애니메이션 CSS 클래스',
+      },
     },
     ariaLabelledby: {
-      control: 'text',
-      description: '모달의 라벨 요소 ID',
+      table: {
+        control: 'text',
+        description: '모달의 라벨 요소 ID',
+      },
     },
     ariaDescribedby: {
-      control: 'text',
-      description: '모달의 설명 요소 ID',
+      table: {
+        control: 'text',
+        description: '모달의 설명 요소 ID',
+      },
     },
   },
   args: {
     isOpen: false,
-    children: ModalContent,
     onClose: fn(),
   },
 };
@@ -78,9 +90,13 @@ const Template = (args: ModalProps) => {
 
   return (
     <div {...args}>
-      <button onClick={() => setIsOpen(true)}>Open Modal</button>
+      <button type="button" onClick={() => setIsOpen(true)}>
+        Open Modal
+      </button>
       <Modal {...args} isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        <div className="bg-white w-32 h-32 rounded-lg p-4">
+        <div
+          style={{ backgroundColor: '#fff', borderRadius: 8, width: 300, height: 300, padding: 20 }}
+        >
           <button type="button" onClick={() => setIsOpen(false)}>
             Close
           </button>
@@ -96,7 +112,6 @@ export const Primary: Story = {
   args: {
     isOpen: false,
     onClose: fn(),
-    children: ModalContent,
   },
 };
 
@@ -124,9 +139,19 @@ export const WithCustomMountPoint: Story = {
 
     return (
       <div>
-        <button onClick={() => setIsOpen(true)}>Open Modal</button>
+        <button type="button" onClick={() => setIsOpen(true)}>
+          Open Modal
+        </button>
         <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} mountNode={{ current: mountNode }}>
-          <div className="bg-white w-32 h-32 rounded-lg p-4">
+          <div
+            style={{
+              backgroundColor: '#fff',
+              borderRadius: 8,
+              width: 300,
+              height: 300,
+              padding: 20,
+            }}
+          >
             <button type="button" onClick={() => setIsOpen(false)}>
               Close
             </button>
